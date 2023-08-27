477394dc451daf903cdc5892169720ebdfc953a5
lycanfloof <lyca2206@gmail.com>
2023-08-27 15:06:56 -0500

    Changed fit method definition in NeuralNetwork. Changed NeuralNetwork's constructor.

--- src/NeuralNetwork.py
+++ src/NeuralNetwork.py
@@ -1,11 +1,10 @@
 import numpy as np
 
 class NeuralNetwork:
-  def __init__(self, cost_function, cost_prime, layers = [], learning_rate = 0.1) -> None:
+  def __init__(self, cost_function, cost_prime, layers = []) -> None:
     self.cost_function = cost_function
     self.cost_prime = cost_prime
     self.layers = layers
-    self.learning_rate = learning_rate
 
   def set_layers(self, layers) -> None:
     self.layers = layers
@@ -13,14 +12,14 @@ class NeuralNetwork:
   def add_layer(self, layer) -> None:
     self.layers.append(layer)
 
-  def fit(self, x_train, y_train, epochs = 50, verbose = False) -> None:
+  def fit(self, x_train, y_train, epochs = 50, verbose = False, learning_rate = 0.1) -> None:
     for epoch in range(epochs):
       output = self.__forward_propagation(x_train)
 
       if verbose: print("Epoch: {} -> Cost: {}".format(epoch + 1, self.cost_function(output, y_train)))
 
       cost = self.cost_prime(output, y_train)
-      self.__backward_propagation(cost)
+      self.__backward_propagation(cost, learning_rate)
 
   def predict(self, x) -> np.ndarray:
     return self.__forward_propagation(x)
@@ -33,8 +32,8 @@ class NeuralNetwork:
 
     return layer_output
 
-  def __backward_propagation(self, cost) -> None:
+  def __backward_propagation(self, cost, learning_rate) -> None:
     current_cost = cost
 
     for layer in reversed(self.layers):
-      current_cost = layer.backward_propagation(current_cost, self.learning_rate)
+      current_cost = layer.backward_propagation(current_cost, learning_rate)
